0549277508386a690289a745db3429f4c28c195c
Tio Gobin <tgobin@gingalab.com>
2018-11-13 14:25:54 +0100

    Erasing all to start again fresh

--- app.js
+++ app.js
@@ -17,15 +17,31 @@ const config = {
 };
 
 const game = new Phaser.Game(config);
+let cursors;
+let player;
+let showDebug = false;
 
 function preload() {
   this.load.image("tiles", "./assets/mapworld/tileset.png");
   this.load.tilemapTiledJSON("map", "./maps/WORLDMAP.json");
 
   this.load.image("chara", "./assets/chara/animations/undead/undead_left_walk.png");
+
+  this.load.spritesheet('undead',
+         'assets/chara/animations/undead/walkcycle_undead.png',
+       { frameWidth: 16, frameHeight: 16 }, 0);
 }
 
 function create() {
+
+  this.add.text(16, 16, 'World Map', {
+      font: "18px monospace",
+      fill: "#000000",
+      padding: { x: 20, y: 10 },
+      backgroundColor: "#ffffff"
+  })
+  .setScrollFactor(0)
+  .setDepth(30);
 
   const map = this.make.tilemap({ key: "map"});
 
@@ -34,29 +50,72 @@ function create() {
   const visual_obstacle = map.createStaticLayer("visual_obstacle", tileset, 0, 0);
   const obstacle = map.createStaticLayer("obstacle", tileset, 0, 0);
   const door = map.createStaticLayer("door", tileset, 0, 0);
+  console.log(obstacle);
 
-  player = this.physics.add.sprite(400, 350, "chara");
+  player = this.physics.add
+    .sprite(350, 450, "undead", "left_walk")
+    .setSize(16, 16)
+    .setOffset(0, 24);
 
- worldLayer.setCollisionByProperty({ collides: true });
+  console.log(player)
+
+  this.physics.add.collider(player, obstacle);
+
+  const anims = this.anims;
+
+  anims.create({
+    key: "left-walk",
+    frames: anims.generateFrameNames("undead", { start: 0, end: 3, zeroPad: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "right-walk",
+    frames: anims.generateFrameNames("undead", { start: 0, end: 3, zeroPad: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "up-walk",
+    frames: anims.generateFrameNames("undead", { start: 0, end: 3, zeroPad: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+  anims.create({
+    key: "down-walk",
+    frames: anims.generateFrameNames("undead", { start: 0, end: 3, zeroPad: 3 }),
+    frameRate: 10,
+    repeat: -1
+  });
+
+  visual_obstacle.setCollisionByProperty({ collides: true });
 }
 
 function update(time, delta) {
+  const speed = 175;
+  const prevVelocity = player.body.velocity.clone();
   // Runs once per frame for the duration of the scene
+  cursors = this.input.keyboard.createCursorKeys();
   player.body.setVelocity(0);
 
   // Horizontal movement
   if (cursors.left.isDown) {
-    player.body.setVelocityX(-100);
+    player.body.setVelocityX(-50);
   } else if (cursors.right.isDown) {
-    player.body.setVelocityX(100);
+    player.body.setVelocityX(50);
   }
 
   // Vertical movement
   if (cursors.up.isDown) {
-    player.body.setVelocityY(-100);
+    player.body.setVelocityY(-50);
   } else if (cursors.down.isDown) {
-    player.body.setVelocityY(100);
+    player.body.setVelocityY(50);
   }
+  if (prevVelocity.x < 0) player.setTexture("undead", "left-walk");
+  else if (prevVelocity.x > 0) player.setTexture("undead", "walkcycle_undead");
+  else if (prevVelocity.y < 0) player.setTexture("undead", "down-walk");
+  else if (prevVelocity.y > 0) player.setTexture("undead", "up-walk");
+
 
   // Normalize and scale the velocity so that player can't move faster along a diagonal
   player.body.velocity.normalize().scale(speed);
